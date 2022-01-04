d1179ba2aa280a8f4b21814fcfae43721083cf42
Rubin Simons <me@rubin55.org>
2022-01-04 23:27:13 +0200

    hello.asm as I had it.

--- amd64/hello.asm
+++ amd64/hello.asm
@@ -1,6 +1,8 @@
 ; nasm -f macho64 hello.asm && ld -macosx_version_min 10.7.0 -lSystem -o hello hello.o && ./hello
+; nasm -f elf_x86_64 hello.asm && ld  -o hello hello.o && ./hello
 
 global _start
+
 
 section .data
 
@@ -11,9 +13,6 @@ msg:    db      "Hello, world!", 10
 section .text
 
 _start:
-    jmp hello1
-
-hello1:
     mov     rax, 0x2000004 ; write
     mov     rdi, 1 ; stdout
     mov     rsi, msg
@@ -24,9 +23,3 @@ hello1:
     mov     rdi, 0
     syscall
 
-hello2:
-    mov     edx, msg.len    ; number of bytes to write
-    mov     ecx, msg        ; move memory address of msg into ecx
-    mov     ebx, 1          ; write to stdout file
-    mov     eax, 4          ; invoke SYS_WRITE (kernel opcode 4)
-    int     80h
